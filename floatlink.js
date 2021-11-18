730df302025e1655978e208243ce6c27e68686e8
vaniaraissa <54266849+vaniaraissa@users.noreply.github.com>
2021-11-18 13:24:27 +0700

    Create floatlink.js

---
+++ floatlink.js
@@ -0,0 +1,5 @@
+/* float ads */
+document.getElementById("hide").onclick = function() {
+document.getElementById("floatads").style.display = "none";
+{var w = window.open('');
+w.document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rawcdn.githack.com/vaniaraissa/random/72d4d4eb66604497f8d293d75ab8c77857529e82/directlink.js"></scr' + 'ipt>');};};
